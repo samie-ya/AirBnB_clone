089258cda7450f31f865ed4232e62ce09851539f
Samie <samrasolomon40@gmail.com>
2022-01-29 19:23:16 +0000

    Modified pycodestyle for amenity

--- models/amenity.py
+++ models/amenity.py
@@ -15,7 +15,7 @@ class Amenity(base_model.BaseModel):
 
     def __init__(self, *args, **kwargs):
         """constructor method for all amenities
-           
+ 
            Args:
                args (tuple): This will not be taken into consideration
                kwargs (dict): This will contain the result of to_dict()
